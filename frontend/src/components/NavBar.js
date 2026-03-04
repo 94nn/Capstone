b1f3e880554fafedb00cd5e23fef868d854d3097
94nn <alinawch1101@gmail.com>
2026-03-04 23:14:56 +0800

    Added a home button

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,19 +1,45 @@
-import React, {useState} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import { NavLink } from 'react-router-dom'
 import './NavBar.css'
 
 const NavBar = () => {
     const [isDropdownVisible, setIsDropdownVisible] = useState(false);
 
+    // Reference for the dropdown and profile container
+    const dropdownRef = useRef(null);
+    const profileContainerRef = useRef(null);
+
     // Toggle the dropdown visibility
     const toggleDropdown = () => {
         setIsDropdownVisible(!isDropdownVisible);
     };
 
+      // Close the dropdown if clicked outside of it
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                dropdownRef.current && !dropdownRef.current.contains(event.target) &&
+                profileContainerRef.current && !profileContainerRef.current.contains(event.target)
+            ) {
+                setIsDropdownVisible(false);
+            }
+        };
+
+        // Add event listener to detect clicks outside
+        document.addEventListener('click', handleClickOutside);
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+        document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
     return (
         <div className='nav-bar'>
             <div className="nav-bar-left">
-                <span className="logo-text">MathDex</span>
+                <NavLink to="/homepage" end className="logo-text">
+                    MathDex
+                </NavLink>
             </div>
             <nav className="nav-bar-items">
                 <NavLink to="/" end className={({ isActive }) => `nav-item ${isActive ? "nav-item-active" : ""}`}>

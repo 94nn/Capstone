6a459a7681bd14601b1be09c7aeb2c3b5d734375
94nn <alinawch1101@gmail.com>
2026-03-04 23:15:10 +0800

    Changed topbar to navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,33 @@ import "./App.css";
 import TopBar from "./components/TopBar";
 import LearnPage from "./pages/LearnPage";
 import SimplePage from "./pages/SimplePage";
+import HomePage from "./components/HomePage";
+import NavBar from "./components/NavBar";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
-                <TopBar />
+                <NavBar />
                     <Routes>
-                    <Route path="/" element={<LearnPage />} />
-                    <Route path="/challenge" element={
-                        <SimplePage
-                            title="Challenge"
-                            description="Challenge"
+                        <Route path="/homepage" element={<HomePage />} />
+            
+                        <Route path="/" element={<LearnPage />} />
+                        
+                        <Route path="/challenge" element={
+                            <SimplePage
+                                title="Challenge"
+                                description="Challenge"
+                            />
+                            }
                         />
-                        }
-                    />
-                    <Route path="/leaderboard" element={
-                        <SimplePage
-                            title="Leaderboard"
-                            description="Leaderboard"
+                        <Route path="/leaderboard" element={
+                            <SimplePage
+                                title="Leaderboard"
+                                description="Leaderboard"
+                            />
+                            }
                         />
-                        }
-                    />
                     </Routes>
             </div>
         </BrowserRouter>

c475ed3011cfcb566609ea9ca4950fa8d4b98244
94nn <alinawch1101@gmail.com>
2026-03-09 21:02:29 +0800

    Add course section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import WlcMessage from '../components/WlcMessage'
-import SidebarProfile from '../components/SidebarProfile'
 import SideProfile from '../components/SideProfile'
 import '../components/HomePage.css'
 import Home_Page_Explore from '../components/Home_Page_Explore'
+import Home_Page_Progress from '../components/Home_Page_Progress'
 
 const HomePage = () => {
   return (
     <main className="main-layout">
       <WlcMessage />
       <SideProfile />
-      <Home_Page_Explore />
+      <div className='left-column'>
+        <Home_Page_Progress />
+        <Home_Page_Explore />
+      </div>
     </main>
   )
 }

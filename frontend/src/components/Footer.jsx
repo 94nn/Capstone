42cbaf25cc5feb788693040dbb804fcd1bf2b206
94nn <alinawch1101@gmail.com>
2026-03-16 01:58:32 +0800

    modify footer content width

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,39 +1,56 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Footer = () => {
-  return (
-    <div className='footer-container'>
-        <div className='footer-header'>
-            <div className='mathdex-header'>
-                <h1>Mathdex</h1>
-                <img src="images/mathdex_logo.png" alt="Mathdex Logo" className='mathdex-logo' />
+    const [modules, setModules] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/modules')
+            .then(res => setModules(res.data))
+            .catch(err => console.error('Error fetching modules:', err))
+    }, [])
+
+    return (
+        <div className='footer-container'>
+            <div className='footer-header'>
+                <div className='mathdex-header'>
+                    <h1>Mathdex</h1>
+                    <img src="images/mathdex_logo.png" alt="Mathdex Logo" className='mathdex-logo' />
+                </div>
+                <div>
+                    <p>Made with ❤️ from 6 bitches</p>
+                </div>
             </div>
-            <div>
-                <p>Made with ❤️ from 6 bitches</p>
+            <div className='footer-content'>
+                <div className='footer-category'>
+                    <p>COMPANY</p>
+                    <Link to="/about" className='footer-links'>About Us</Link>
+                </div>
+                <div className='footer-category'>
+                    <p>PRACTICE</p>
+                    <Link to="/challenges" className='footer-links'>Challenges</Link>
+                </div>
+                <div className='footer-category'>
+                    <p>LEARN</p>
+                    <div className='footer-links-container'>
+                        {modules.map(module => (
+                            <Link 
+                                key={module.id}
+                                to={`/module/${module.slug}`} 
+                                className='footer-links'
+                            >
+                                {module.name}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className='copyright'>
+                <p className='text-center text-sm text-gray-500 py-4'>© 2026 Mathdex. All rights reserved.</p>
             </div>
         </div>
-        <div className='footer-content'>
-            <div className='footer-category'>
-                <p>COMPANY</p>
-                <Link to="/about" className='footer-links'>About Us</Link>
-            </div>
-            <div className='footer-category'>
-                <p>PRACTICE</p>
-                <Link to="/challenges" className='footer-links'>Challenges</Link>
-            </div>
-            <div className='footer-category'>
-                <p>LEARN</p>
-                <Link to="/module/differentiation" className='footer-links'>Differentiation</Link>
-                <Link to="/module/integration" className='footer-links'>Integration</Link>
-                <Link to="/module/algebra" className='footer-links'>Algebra</Link>
-            </div>
-        </div>
-        <div className='copyright'>
-            <p className='text-center text-sm text-gray-500 py-4'>© 2026 Mathdex. All rights reserved.</p>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Footer

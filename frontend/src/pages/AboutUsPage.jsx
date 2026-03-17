93b0df1711c8fb1d6687c005a6f2062cd1f26a6f
94nn <alinawch1101@gmail.com>
2026-03-17 14:55:36 +0800

    Initial commit

---
+++ frontend/src/pages/AboutUsPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const AboutUsPage = () => {
+  return (
+    <div>
+        <div className="about-us-image">
+            <img src="" alt="" />
+        </div>
+        <div>
+            <h1>THE MOST FUN WAY TO LEARN MATH!!</h1>
+            <div>
+                <p>Mathdex is a brand new way to learn math online. Journey through the world of Algebra, Geometry, Calculus, Statistics, or Trigonometry. Earn experience points (XP) to unlock new levels, solve challenges, and collect all the badges at your own pace.</p>
+                <p>(づ｡◕‿‿◕｡)づ:･ﾟ✧</p>
+            </div>
+        </div>
+    </div>
+  )
+}

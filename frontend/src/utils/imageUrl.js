300bf29961802e3730ac8682579896716196e488
94nn <alinawch1101@gmail.com>
2026-04-08 11:05:24 +0800

    remove duplicate getInitials

--- frontend/src/utils/imageUrl.js
+++ frontend/src/utils/imageUrl.js
@@ -10,8 +10,3 @@ export function getInitials(name) {
     if (!name) return '?'
     return name.split(' ').map(w => w[0]).join('').slice(0, 2).toUpperCase()
 }
-
-export function getInitials(name) {
-    if (!name) return '?'
-    return name.split(' ').map(w => w[0]).join('').slice(0, 2).toUpperCase()
-}
